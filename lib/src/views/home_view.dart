c281412f08eff388044d5d4ed806fabcf1519111
Arslan Qazi <arslanqazi1999@gmail.com>
2026-08-19 11:37:47 +0500

    Update home_view.dart

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -11,33 +11,67 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: Column(children: [
+    Responsive.init(context);
 
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+    return SafeArea(
+      child: Scaffold(
+        endDrawer: Responsive.isMobile
+            ? Drawer(
+          child: ListView(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
             children: [
-            BlackText(
-              onTap: (){},
-              text: "Works",
-            ),
-            const SizedBox(width: 20),
-            BlackText(
-              onTap: (){},
-              text: "Blog",
-            ),
-            const SizedBox(width: 20),
-            BlackText(
-              onTap: (){},
-              text: "Contact",
-            ),
-          ],),
+              BlackText(
+                text: "Works",
+                fontSize: 18,
+                onTap: () => Navigator.pop(context),
+              ),
+              const SizedBox(height: 20),
+              BlackText(
+                text: "Blog",
+                fontSize: 18,
+                onTap: () => Navigator.pop(context),
+              ),
+              const SizedBox(height: 20),
+              BlackText(
+                text: "Contact",
+                fontSize: 18,
+                onTap: () => Navigator.pop(context),
+              ),
+            ],
+          ),
         )
-
-
-      ],),
-    ));
+            : null,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  if (Responsive.isMobile)
+                    Builder(
+                      builder: (context) => IconButton(
+                        icon: const Icon(Icons.menu, color: Colors.black, size: 28),
+                        onPressed: () => Scaffold.of(context).openEndDrawer(),
+                      ),
+                    )
+                  else
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        BlackText(onTap: () {}, text: "Works"),
+                        const SizedBox(width: 20),
+                        BlackText(onTap: () {}, text: "Blog"),
+                        const SizedBox(width: 20),
+                        BlackText(onTap: () {}, text: "Contact"),
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
